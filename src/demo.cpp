d7364d292be58f59e4d876bb6619f3d18bf39429
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2016-05-04 07:48:05 +0200

    adjustments for post-3.0.2 ffmpeg api changes

--- src/demo.cpp
+++ src/demo.cpp
@@ -44,6 +44,41 @@ std::string translateErrorCode(int ec)
     return std::string(buffer);
 }
 
+void ffmpegLogHandler(void* avcl, int level, char const* fmt, va_list vl)
+{
+    int print_prefix = 1;
+    auto const buffer_size = av_log_format_line2(avcl, level, fmt, vl, nullptr, 0, &print_prefix);
+    std::vector<char> buffer(buffer_size + 1);
+    av_log_format_line2(avcl, level, fmt, vl, buffer.data(), buffer_size, &print_prefix);
+    Ghulbus::LogLevel log_level;
+    switch(level) {
+    default:
+    case AV_LOG_PANIC:
+    case AV_LOG_FATAL:
+        log_level = Ghulbus::LogLevel::Critical;
+        break;
+    case AV_LOG_ERROR:
+        log_level = Ghulbus::LogLevel::Error;
+        break;
+    case AV_LOG_WARNING:
+        log_level = Ghulbus::LogLevel::Warning;
+        break;
+    case AV_LOG_INFO:
+        log_level = Ghulbus::LogLevel::Info;
+        break;
+    case AV_LOG_VERBOSE:
+    case AV_LOG_DEBUG:
+        log_level = Ghulbus::LogLevel::Debug;
+        break;
+    case AV_LOG_TRACE:
+        log_level = Ghulbus::LogLevel::Trace;
+        break;
+    }
+    // remove trailing '\n' if any; GHULBUS_LOG will always linebreak after each message by itself
+    if(buffer[buffer_size - 2] == '\n') { buffer[buffer_size - 2] = '\0' ; }
+    GHULBUS_LOG_QUALIFIED(log_level, "[FFMPEG] " << buffer.data());
+}
+
 int main()
 {
     Ghulbus::Log::initializeLogging();
@@ -56,41 +91,7 @@ int main()
 
     av_register_all();
     avformat_network_init();
-
-    av_log_set_callback([](void* avcl, int level, char const* fmt, va_list vl) {
-        auto const buffer_size = snprintf(nullptr, 0, fmt, vl) + 1;
-        std::vector<char> buffer(buffer_size);
-        snprintf(buffer.data(), buffer_size, fmt, vl);
-        Ghulbus::LogLevel log_level;
-        switch(level) {
-        default:
-        case AV_LOG_PANIC:
-        case AV_LOG_FATAL:
-            log_level = Ghulbus::LogLevel::Critical;
-            break;
-        case AV_LOG_ERROR:
-            log_level = Ghulbus::LogLevel::Error;
-            break;
-        case AV_LOG_WARNING:
-            log_level = Ghulbus::LogLevel::Warning;
-            break;
-        case AV_LOG_INFO:
-            log_level = Ghulbus::LogLevel::Info;
-            break;
-        case AV_LOG_VERBOSE:
-        case AV_LOG_DEBUG:
-            log_level = Ghulbus::LogLevel::Debug;
-            break;
-        case AV_LOG_TRACE:
-            log_level = Ghulbus::LogLevel::Trace;
-            break;
-        }
-        GHULBUS_LOG_QUALIFIED(log_level, "[FFMPEG] " << buffer.data());
-    });
-
-    av_log_set_callback(av_log_default_callback);
-    av_log(nullptr, AV_LOG_ERROR, "Foo");
-
+    av_log_set_callback(ffmpegLogHandler);
     std::unique_ptr<AVFormatContext, void(*)(AVFormatContext*)> format_context_storage(avformat_alloc_context(),
                                                                                        &avformat_free_context);
     auto format_context = format_context_storage.get();
@@ -117,8 +118,8 @@ int main()
     int play = -1;
     for(unsigned int i=0; i<format_context->nb_streams; ++i) {
         AVStream* it = format_context->streams[i];
-        GHULBUS_LOG(Info, "#" << i << " " << it->codec->codec_type);
-        if(it->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
+        GHULBUS_LOG(Info, "#" << i << " " << it->codecpar->codec_type);
+        if(it->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
             play = i;
         }
     }
@@ -128,7 +129,7 @@ int main()
     int const length = static_cast<int>(stream->duration / stream->time_base.den) * stream->time_base.num;
     GHULBUS_LOG(Info, "Length: " << (length / 60) << ":" << ((length % 60 < 10) ? "0" : "") << (length % 60));
 
-    auto codec = avcodec_find_decoder(stream->codec->codec_id);
+    auto codec = avcodec_find_decoder(stream->codecpar->codec_id);
     if(codec == 0) {
         GHULBUS_LOG(Error, "Invalid codec.");
     }
@@ -136,14 +137,7 @@ int main()
     auto codec_context = std::unique_ptr<AVCodecContext, void(*)(AVCodecContext*)>(avcodec_alloc_context3(codec),
         [](AVCodecContext* ctx) { avcodec_free_context(&ctx); });
 
-    auto res = avcodec_copy_context(codec_context.get(), format_context->streams[play]->codec);
-    if(res != 0) {
-        char buffer[AV_ERROR_MAX_STRING_SIZE];
-        av_strerror(res, buffer, sizeof(buffer));
-        GHULBUS_LOG(Error, "Unable to copy codec context: " << buffer);
-    }
-
-    res = avcodec_open2(codec_context.get(), codec, nullptr);
+    auto res = avcodec_open2(codec_context.get(), codec, nullptr);
     if(res != 0) {
         GHULBUS_LOG(Error, "Error opening codec: " << translateErrorCode(res));
     }
@@ -175,37 +169,42 @@ int main()
     }
 
     std::vector<char> raw_data;
-    int got_frame = 0;
     while(av_read_frame(format_context, &packet) == 0) {
         auto const packet_guard =
             gsl::finally([packet]() mutable { av_packet_unref(&packet); });     // capture by-value here! packet will
                                                                                 // get overwritten by decode below
-        do {
-            res = avcodec_decode_audio4(codec_context.get(), frame.get(), &got_frame, &packet);
-            if(res < 0) {
-                GHULBUS_LOG(Error, "Error during decoding: " << translateErrorCode(res));
-                break;
+        res = avcodec_send_packet(codec_context.get(), &packet);
+        if(res != 0) {
+            GHULBUS_LOG(Error, "Codec send packet error: " << res);
+            break;
+        }
+        for(;;) {
+            res = avcodec_receive_frame(codec_context.get(), frame.get());
+            if(res != 0) {
+                if(res == AVERROR(EAGAIN)) {
+                    // everything we read before was decoded; read more stuff and call send_packet again
+                    break;
+                } else {
+                    GHULBUS_LOG(Error, "Codec receive frame error: " << res);
+                    break;
+                }
             }
-            auto const decoded = std::min(res, packet.size);
-            if(got_frame) {
-                // something was actually decoded! w00t!
-                GHULBUS_LOG(Trace, "Decoded " << frame->nb_samples << " samples.");
-            }
-            packet.data += decoded;
-            packet.size -= decoded;
-        } while(packet.size > 0);
-        if(res < 0) { break; }
+        }
     }
 
     // flush cached frames
-    packet.data = nullptr;
-    packet.size = 0;
-    do {
-        do {
-            res = avcodec_decode_audio4(codec_context.get(), frame.get(), &got_frame, &packet);
-            if(res < 0) { break; }
-        } while(packet.size > 0);
-    } while(got_frame);
+    avcodec_send_packet(codec_context.get(), nullptr);
+    for(;;) {
+        res = avcodec_receive_frame(codec_context.get(), frame.get());
+        if(res == AVERROR_EOF) {
+            GHULBUS_LOG(Info, "Done decoding.");
+            break;
+        } else {
+            GHULBUS_LOG(Error, "Codec receive frame error: " << res);
+            break;
+        }
+    }
+
 
     top_level.stop();
     Ghulbus::Log::shutdownLogging();
