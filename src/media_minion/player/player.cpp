115d9dc3110b25a4edff56e0006f94b6f5f25842
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-08-03 11:25:19 +0200

    masked qt5 warnings

--- src/media_minion/player/player.cpp
+++ src/media_minion/player/player.cpp
@@ -1,6 +1,13 @@
 #include <media_minion/player/ui/tray_icon.hpp>
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4251)
+#endif
 #include <QApplication>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 int main(int argc, char* argv[])
 {
--- src/media_minion/player/ui/tray_icon.hpp
+++ src/media_minion/player/ui/tray_icon.hpp
@@ -1,7 +1,14 @@
 #ifndef MEDIA_MINION_INCLUDE_GUARD_PLAYER_UI_TRAY_ICON_HPP_
 #define MEDIA_MINION_INCLUDE_GUARD_PLAYER_UI_TRAY_ICON_HPP_
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 5054)
+#endif
 #include <QSystemTrayIcon>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 namespace media_minion::player::ui {
 
